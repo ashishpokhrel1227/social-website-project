e4770b12d24c3028e45f968bfa07ed7b6c49abd1
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-04-30 22:32:14 +0545

    little more on authentication

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
                 # previous login view
                 # path('login/', views.user_login, name='login'),
-                path('login/', auth_views.LoginView.as_view(), name='login')
-                path('logout/', auth_views.LogoutView.as_view(), name='logout')
+                path('login/', auth_views.LoginView.as_view(), name='login'),
+                path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+                path('', views.dashboard, name='dashboard'),
 ]
