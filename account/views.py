cc40a755baff17bebf5871a80f6cfcd73298fd92
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-01 21:29:16 +0545

    use of message framework for user profile

--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from .forms import LoginForm, UserRegistrationForm, \
                     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-
+from django.contrib import messages
 
 def user_login(request):
     if request.method == 'POST':
@@ -62,6 +62,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                        'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
