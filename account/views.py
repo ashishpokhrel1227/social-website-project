471e6b7b7090e8611b11b7c36fe7e1611c1ee83f
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-01 16:37:27 +0545

    creation of user register for the site

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 def user_login(request):
@@ -31,3 +31,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+            user_form = UserRegistrationForm(request.POST)
+            if user_form.is_valid():
+                #create a new user object but avoid saving it yet
+                new_user = user_form.save(commit=False)
+                #set the choosen password
+                new_user.set_password(user_form.cleaned_data['password'])
+                #save the user object
+                new_user.save()
+                return render(request, 'account/register_done.html', {'new_user': new_user})
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
