a56b4918c62131c70ab4d135c25b7405ba32c1d1
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-05 13:54:21 +0545

    a new view showing image for sharing on the site

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -17,3 +20,17 @@ class ImageCreateForm(forms.ModelForm):
                                         'match valid image extensions.')
 
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+
+        #download image from the given url
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()),
+                            save=False)
+
+        if commit:
+            image.save()
+        return image
