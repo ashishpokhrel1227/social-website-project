6e82c02c32e833b3f5b11e82f6b05c3a45e974ea
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-05 11:10:47 +0545

    registering the image model in the administration site

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #3rd party app
     'social_django',
+    'images.apps.ImagesConfig',
 
 ]
 
@@ -65,12 +66,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
 
 AUTHENTICATION_BACKENDS = [
+'social_core.backends.google.GoogleOAuth2',
 'social_core.backends.facebook.FacebookOAuth2',
 'django.contrib.auth.backends.ModelBackend',
 'account.authentication.EmailAuthBackend',
@@ -112,7 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 SOCIAL_AUTH_FACEBOOK_KEY = '396367514288847'
 SOCIAL_AUTH_FACEBOOK_SECRET = '3e42512c0a81251928ef66d9e3e8e99d'
-
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '906950487707-9kbfpnhg3v6biksh7sso8pk2h90qkr0h.apps.googleusercontent.com' # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'YxTMFAzkvn3Ci6IcCDc00yGA' # Google Consumer Secret
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
