d83d236d18f53b151319394dd89455d1ce613ad1
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-05 16:59:06 +0545

    detail of image bookmarked using bookmarklet is created with showing like count

--- images/views.py
+++ images/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+
 
 @login_required
 def image_create(request):
@@ -24,3 +27,9 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    
